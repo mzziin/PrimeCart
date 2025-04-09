9c46d28088187f6a1e76e30b4640c992de3d73d6
Mohammed Mazin <muhammedmazin38@gmail.com>
2025-04-09 14:06:54 +0530

    configure fluent api for domain models in dbcontext

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -12,27 +12,18 @@ public class AppDbContext : DbContext
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Seller> Sellers { get; set; }
     public DbSet<Address> Addresses { get; set; }
-
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
-
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
-
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
     public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
-
     public DbSet<Wishlist> Wishlists { get; set; }
     public DbSet<WishlistItem> WishlistItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        // Convert status type enum in OrderItem to string
-        modelBuilder.Entity<OrderItem>()
-            .Property(o => o.Status)
-            .HasConversion<string>();
 
         // --- Customer ---
         modelBuilder.Entity<Customer>(entity =>
@@ -86,21 +77,119 @@ public class AppDbContext : DbContext
 
             entity.HasOne(p => p.Category)
                 .WithMany(c => c.Products)
-                .HasForeignKey(c => c.CategoryId);
+                .HasForeignKey(p => p.CategoryId);
 
             // Avoid soft deleted products globally while querying
             entity.HasQueryFilter(p => !p.IsDeleted);
         });
-
+        
         // --- Address ---
         modelBuilder.Entity<Address>(entity =>
         {
             entity.ToTable("Addresses");
             entity.HasKey(a => a.Id);
+            
+            entity.Property(a=>a.Street).HasMaxLength(100);
+            entity.Property(a=>a.City).HasMaxLength(100);
+            entity.Property(a=>a.State).HasMaxLength(100);
+            entity.Property(a=>a.Country).HasMaxLength(100);
 
             entity.HasOne(a => a.customer)
                 .WithMany(c => c.Addresses)
                 .HasForeignKey(a => a.customerId);
+        });
+        
+        // --- Category ---
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("Categories");
+            entity.HasKey(c => c.Id);
+
+            entity.Property(c => c.Name).HasMaxLength(100);
+        });
+        
+        // --- Order ---
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.ToTable("Orders");
+            entity.HasKey(o => o.Id);
+            
+            entity.Property(o=>o.TotalAmount).HasColumnType("Decimal(18,2)");
+
+            entity.HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId);
+        });
+        
+        // --- OrderItem ---
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.ToTable("OrderItems");
+            entity.HasKey(o => o.Id);
+            
+            // Convert status type enum in OrderItem to string
+            entity.Property(o => o.Status).HasConversion<string>();
+            entity.Property(o => o.Price).HasColumnType("decimal(18,2)");
+
+            entity.HasOne(o => o.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(o => o.ProductId);
+            
+            entity.HasOne(o => o.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(o => o.OrderId);
+        });
+        
+        // --- Shopping Cart ---
+        modelBuilder.Entity<ShoppingCart>(entity =>
+        {
+            entity.ToTable("ShoppingCarts");
+            entity.HasKey(c => c.Id);
+
+            entity.HasOne(s => s.Customer)
+                .WithOne(c => c.ShoppingCart)
+                .HasForeignKey<ShoppingCart>(c => c.CustomerId);
+        });
+        
+        // --- ShoppingCart Items ---
+        modelBuilder.Entity<ShoppingCartItem>(entity =>
+        {
+            entity.ToTable("ShoppingCarts");
+            entity.HasKey(c => c.Id);
+            
+            entity.HasOne(s=>s.Cart)
+                .WithMany(c=>c.ShoppingCartItems)
+                .HasForeignKey(s=>s.CartId);
+            
+            entity.HasOne(s=>s.Product)
+                .WithOne(p=>p.ShoppingCartItem)
+                .HasForeignKey<ShoppingCartItem>(s=>s.ProductId);
+        });
+        
+        // --- Wishlist ---
+        modelBuilder.Entity<Wishlist>(entity =>
+        {
+            entity.ToTable("Wishlists");
+            entity.HasKey(c => c.Id);
+            
+            entity.HasOne(s => s.Customer)
+                .WithOne(c => c.Wishlist)
+                .HasForeignKey<Wishlist>(c => c.CustomerId);
+        });
+        
+        // --- Wishlist Items ---
+        modelBuilder.Entity<WishlistItem>(entity =>
+        {
+            entity.ToTable("WishlistItems");
+            entity.HasKey(c => c.Id);
+
+            entity.HasOne(s => s.Product)
+                .WithMany(w => w.WishlistItems)
+                .HasForeignKey(s => s.ProductId);
+            
+            entity.HasOne(w=>w.Wishlist)
+                .WithMany(w=>w.WishlistItems)
+                .HasForeignKey(w=>w.WishlistId);
         });
     }
 }
