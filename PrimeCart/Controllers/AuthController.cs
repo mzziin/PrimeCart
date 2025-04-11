7811b4847e2e123d09d56398dd94a3a93596a926
Mohammed Mazin <muhammedmazin38@gmail.com>
2025-04-11 23:49:46 +0530

    Create and implement auth controller

---
+++ PrimeCart/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using Application.DTOs;
+using Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeCart.Controllers
+{
+
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("/customer/register")]
+        public async Task<IActionResult> Register([FromBody] RegisterCustomer registerCustomer)
+        {
+            var result = await _authService.RegisterCustomer(registerCustomer);
+            if (result == 2) return Ok(result);
+
+            return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        [HttpPost("/seller/register")]
+        public async Task<IActionResult> RegisterSeller([FromBody] RegisterSeller registerSeller)
+        {
+            var result = await _authService.RegisterSeller(registerSeller);
+            if (result == 2) return Ok(result);
+            
+            return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        [HttpPost("/user/login")]
+        public async Task<IActionResult> Login([FromBody] LoginUser loginUser)
+        {
+            var token = await _authService.Login(loginUser);
+            if (token == "") return NotFound();
+            return Ok(token);
+        }
+    }
+}
